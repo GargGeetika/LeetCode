e59ed39870b2ce54695c5e3bf908143893d1cd04
GargGeetika <31517666+GargGeetika@users.noreply.github.com>
2025-07-15 17:23:01 -0400

    Create validParentheses.js
    validate if the string with parentheses is valid.

---
+++ validParentheses.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack=[]
+    const map={
+        '(':')',
+        '{':'}',
+        '[':']'
+    }
+
+    for(let char of s){
+
+        if (map[char]) stack.push(map[char])
+            else if(stack.pop()!==char) return false
+    }
+    return stack.length === 0;
+};
+
+let paran=isValid('{[(p)]}')
+console.log(paran)
